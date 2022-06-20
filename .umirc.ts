0c85fef6a33cb85adf863171fb1480136bc8e45d
花果山大圣 <316783812@qq.com>
2022-06-20 20:47:50 +0800

    chore: update icon

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'umaso',
   favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+    'https://cdn.jsdelivr.net/gh/shengxinjing/static/pangxie.png',
+  logo: 'https://cdn.jsdelivr.net/gh/shengxinjing/static/pangxie.png',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
