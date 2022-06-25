ba1a564c61d1f88c3e71f808846c33b0708cd0e9
aiyoudiao <newdiao@163.com>
2022-06-25 22:29:45 +0800

    feat(container): 提交contianer组件、测试、文档。

--- src/components/container/index.test.tsx
+++ src/components/container/index.test.tsx
@@ -1,16 +1,159 @@
-// import React from 'react'
-// import type { ReactTestRendererJSON } from 'react-test-renderer'
-// import renderer from 'react-test-renderer'
+import React from 'react'
+import type { ReactTestRendererJSON } from 'react-test-renderer'
+import renderer from 'react-test-renderer'
 import { describe, expect, it } from 'vitest'
-// import { ElHeader, ElAside, ElMain, ElFooter, ElContainer } from './index'
+import { ElAside, ElContainer, ElFooter, ElHeader, ElMain } from './index'
 
-describe('ElHeader', () => {
-  it('ElHeader render', () => {
-    // const component = renderer.create(
-    //   <ElHeader></ElHeader>,
-    // )
-    // const { type } = component.toJSON() as ReactTestRendererJSON
-    // expect(type).toBe('el-header')
-    expect(1).toBe(1)
+describe('ElHeader.react', () => {
+  it('Header snapshot', () => {
+    const component = renderer.create(<ElHeader></ElHeader>)
+    expect(component).toMatchSnapshot()
+  })
+  it('Header render', () => {
+    const component = renderer.create(
+      <ElHeader></ElHeader>,
+    )
+    const { type, props: { className, name } } = component.toJSON() as ReactTestRendererJSON
+    expect(type).toBe('header')
+    expect(className).toBe('el-header')
+    expect(name).toBe('el-header')
+  })
+  it('Header chldren', () => {
+    const text = 'hello world'
+    const component = renderer.create(<ElHeader>{text}</ElHeader>)
+    const { children } = component.toJSON() as ReactTestRendererJSON
+    expect(children).toContain(text)
+  })
+  it('Header style', () => {
+    const component = renderer.create(<ElHeader height="90px"></ElHeader>)
+    const { props: { style } } = component.toJSON() as ReactTestRendererJSON
+    expect(style).toEqual({ height: '90px' })
+  })
+})
+
+describe('ElAside.react', () => {
+  it('Aside snapshot', () => {
+    const component = renderer.create(<ElAside></ElAside>)
+    expect(component).toMatchSnapshot()
+  })
+  it('Aside render', () => {
+    const component = renderer.create(
+      <ElAside></ElAside>,
+    )
+    const { type, props: { className, name } } = component.toJSON() as ReactTestRendererJSON
+    expect(type).toBe('aside')
+    expect(className).toBe('el-aside')
+    expect(name).toBe('el-aside')
+  })
+  it('Aside chldren', () => {
+    const text = 'hello world'
+    const component = renderer.create(<ElAside>{text}</ElAside>)
+    const { children } = component.toJSON() as ReactTestRendererJSON
+    expect(children).toContain(text)
+  })
+  it('Aside style', () => {
+    const component = renderer.create(<ElAside width="250px"></ElAside>)
+    const { props: { style } } = component.toJSON() as ReactTestRendererJSON
+    expect(style).toEqual({ width: '250px' })
+  })
+})
+
+describe('ElMain.react', () => {
+  it('Main snapshot', () => {
+    const component = renderer.create(<ElMain></ElMain>)
+    expect(component).toMatchSnapshot()
+  })
+  it('Main render', () => {
+    const component = renderer.create(<ElMain></ElMain>)
+    const { type, props: { className, name } } = component.toJSON() as ReactTestRendererJSON
+    expect(type).toBe('main')
+    expect(className).toBe('el-main')
+    expect(name).toBe('el-main')
+  })
+  it('Main children', () => {
+    const text = 'hello world'
+    const component = renderer.create(<ElMain>{text}</ElMain>)
+    const { children } = component.toJSON() as ReactTestRendererJSON
+    expect(children).toContain(text)
+  })
+})
+
+describe('ElFooter.react', () => {
+  it('Footer snapshot', () => {
+    const component = renderer.create(<ElFooter></ElFooter>)
+    expect(component).toMatchSnapshot()
+  })
+  it('Footer render', () => {
+    const component = renderer.create(
+      <ElFooter></ElFooter>,
+    )
+    const { type, props: { className, name } } = component.toJSON() as ReactTestRendererJSON
+    expect(type).toBe('footer')
+    expect(className).toBe('el-footer')
+    expect(name).toBe('el-footer')
+  })
+  it('Footer chldren', () => {
+    const text = 'hello world'
+    const component = renderer.create(<ElFooter>{text}</ElFooter>)
+    const { children } = component.toJSON() as ReactTestRendererJSON
+    expect(children).toContain(text)
+  })
+  it('Footer style', () => {
+    const component = renderer.create(<ElFooter height="90px"></ElFooter>)
+    const { props: { style } } = component.toJSON() as ReactTestRendererJSON
+    expect(style).toEqual({ height: '90px' })
+  })
+})
+
+describe('ElContainer.react', () => {
+  it ('Container snapshot', () => {
+    const component = renderer.create(<ElContainer></ElContainer>)
+    expect(component).toMatchSnapshot()
+  })
+
+  describe('Container class', () => {
+    it('Container has class el-container', () => {
+      const component = renderer.create(<ElContainer></ElContainer>)
+      const { props: { className } } = component.toJSON() as ReactTestRendererJSON
+      expect(className).toBe(className)
+    })
+    it('Container has class is-vertical by prop direction=vertical', () => {
+      const component = renderer.create(<ElContainer direction="vertical"></ElContainer>)
+      const { props: { className } } = component.toJSON() as ReactTestRendererJSON
+      expect(className).toContain('is-vertical')
+    })
+    it('Container has class is-vertical by prop direction=horizontal', () => {
+      const component = renderer.create(<ElContainer direction="horizontal"></ElContainer>)
+      const { props: { className } } = component.toJSON() as ReactTestRendererJSON
+      expect(className.includes('is-vertical')).toBeFalsy()
+      expect(className.includes('el-container')).toBeTruthy()
+    })
+  })
+
+  describe('Container children', () => {
+    it('chldren has el-header', () => {
+      const component = renderer.create(
+        <ElContainer>
+          <ElHeader></ElHeader>
+          <ElMain>
+            <ElAside>aside</ElAside>
+          </ElMain>
+        </ElContainer>,
+      )
+      const { props: { className } } = component.toJSON() as ReactTestRendererJSON
+      expect(className).toContain('is-vertical')
+    })
+    it('chldren has el-footer', () => {
+      const component = renderer.create(
+        <ElContainer>
+          <ElMain>
+            <ElAside>aside</ElAside>
+          </ElMain>
+          <ElFooter></ElFooter>
+        </ElContainer>,
+      )
+      const { props: { className } } = component.toJSON() as ReactTestRendererJSON
+      expect(className).toContain('is-vertical')
+    })
   })
 })
--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,5 +1,5 @@
-import type { ReactNode } from 'react'
-import React, { forwardRef } from 'react'
+import type { FC, ReactNode } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 import '../../theme/src/header.scss'
 import '../../theme/src/aside.scss'
@@ -7,21 +7,20 @@ import '../../theme/src/main.scss'
 import '../../theme/src/footer.scss'
 import '../../theme/src/container.scss'
 
-export enum DirectionEnum {
+enum DirectionEnum {
   horizontal = 'horizontal',
   vertical = 'vertical',
 }
 
 export interface ContainerProps {
-  prefixClass: string
-  className: string
-  children: ReactNode
-  direction?: DirectionEnum.horizontal | DirectionEnum.vertical
+  className?: string
+  children?: ReactNode
+  direction?: DirectionEnum.horizontal | DirectionEnum.vertical | string
   width?: string
   height?: string
 }
 
-export enum TagEnum {
+enum TypeEnum {
   header = 'el-header',
   aside = 'el-aside',
   main = 'el-main',
@@ -29,94 +28,123 @@ export enum TagEnum {
   container = 'el-container',
 }
 
-export type TagType = TagEnum.header | TagEnum.aside | TagEnum.main | TagEnum.footer | TagEnum.container
-export interface ContainerPropsWithTagName extends ContainerProps {
-  tagName: TagType
+export type TagType = TypeEnum.header | TypeEnum.aside | TypeEnum.main | TypeEnum.footer | TypeEnum.container
+export interface ContainerPropsWithTypeName extends ContainerProps {
+  typeName: TagType
+  tagName: string
 }
 
-const SubContainer = forwardRef<HTMLElement, ContainerPropsWithTagName>((props, ref) => {
-  const { prefixClass, className: oldClassName, children, tagName, ...otherList } = props
-  const className = classNames(prefixClass, oldClassName)
+const SubContainer: FC<ContainerPropsWithTypeName> = (props) => {
+  const { className: oldClassName, children, tagName, typeName, ...otherList } = props
+  const className = classNames(typeName, oldClassName)
   return React.createElement(
     tagName,
     {
       className,
       ...otherList,
-      ref,
+      name: typeName,
     },
     children,
   )
-})
+}
 
-const Container = forwardRef<HTMLElement, ContainerPropsWithTagName>((props, ref) => {
-  const { prefixClass, className: oldClassName, children, tagName, direction, ...otherList } = props
+const Container: FC<ContainerPropsWithTypeName> = (props) => {
+  const { className: oldClassName, children, tagName, direction, typeName, ...otherList } = props
   const list = Array.isArray(children) ? children : [children]
-  const isVertical = list.some(item => ['el-header', 'el-footer'].includes(item.name))
+  const isVertical = list?.some((item: React.ReactComponentElement<any>) => {
+    if (item && item.type) {
+      const { props: { typeName } } = item.type(item.props)
+      return ['el-header', 'el-footer'].includes(typeName)
+    }
+    return false
+  })
 
   let className = ''
   if (isVertical || direction === DirectionEnum.vertical)
-    className = classNames(prefixClass, oldClassName, 'is-vertical')
+    className = classNames(typeName, oldClassName, 'is-vertical')
   else
-    className = classNames(prefixClass, oldClassName)
+    className = classNames(typeName, oldClassName)
 
   return React.createElement(
     tagName,
     {
       className,
       ...otherList,
-      ref,
+      name: typeName,
     },
     children,
   )
-})
+}
 
-function generateComponent(tagName: TagType) {
-  /**
-   * TODO
-   * 1. 如果是header footer 组件，就给他设置height: 60px
-   * 2. 如果是 aside 组件，就给他设置width：300px
-   * 3. 如果是 container组件，就给他设置direction: horizontal
-   */
-  switch (tagName) {
-    case TagEnum.header:
-    case TagEnum.footer: {
+function generateComponent(tagName: string, typeName: TypeEnum) {
+  switch (typeName) {
+    case TypeEnum.header:
+    case TypeEnum.footer: {
       return (MyComponent: any) => {
-        const Adapter = React.forwardRef<HTMLElement, ContainerProps>((props, ref) => {
+        const Adapter: FC<ContainerProps> = (props) => {
           const { height = '60px', ...otherList } = props
-          return <MyComponent ref={ref} styles={{ height }} {...otherList} />
-        })
+          const newPorps = {
+            ...otherList,
+            style: { height },
+            tagName,
+            typeName,
+          }
+          return <MyComponent {...newPorps} />
+        }
         return Adapter
       }
     }
-    case TagEnum.aside: {
+    case TypeEnum.aside: {
       return (MyComponent: any) => {
-        const Adapter = React.forwardRef<HTMLElement, ContainerProps>((props, ref) => {
+        const Adapter: FC<ContainerProps> = (props) => {
           const { width = '300px', ...otherList } = props
-          return <MyComponent ref={ref} styles={{ width }} {...otherList} />
-        })
+          const newPorps = {
+            ...otherList,
+            style: { width },
+            tagName,
+            typeName,
+          }
+          return <MyComponent {...newPorps} />
+        }
         return Adapter
       }
     }
-    case TagEnum.main: {
-      return (MyComponent: any) => MyComponent
-    }
-    case TagEnum.container: {
+    case TypeEnum.main: {
       return (MyComponent: any) => {
-        const Adapter = React.forwardRef<HTMLElement, ContainerProps>((props, ref) => {
+        const Adapter: FC<ContainerProps> = (props) => {
+          const newPorps = {
+            ...props,
+            typeName,
+            tagName,
+          }
+          return <MyComponent {...newPorps} />
+        }
+        return Adapter
+      }
+    }
+    case TypeEnum.container: {
+      return (MyComponent: any) => {
+        const Adapter: FC<ContainerProps> = (props) => {
           const { direction = DirectionEnum.horizontal, ...otherList } = props
-          return <MyComponent ref={ref} { ...{ direction } } {...otherList} />
-        })
+          const newPorps = {
+            ...otherList,
+            typeName,
+            tagName,
+            direction,
+          }
+          return <MyComponent {...newPorps} />
+        }
         return Adapter
       }
     }
   }
 }
 
-const ElContainer = generateComponent(TagEnum.container)(Container)
-const ElHeader = generateComponent(TagEnum.header)(SubContainer)
-const ElAside = generateComponent(TagEnum.aside)(SubContainer)
-const ElMain = generateComponent(TagEnum.main)(SubContainer)
-const ElFooter = generateComponent(TagEnum.footer)(SubContainer)
+const ElContainer = generateComponent('section', TypeEnum.container)(Container)
+const ElHeader = generateComponent('header', TypeEnum.header)(SubContainer)
+const ElAside = generateComponent('aside', TypeEnum.aside)(SubContainer)
+const ElMain = generateComponent('main', TypeEnum.main)(SubContainer)
+const ElFooter = generateComponent('footer', TypeEnum.footer)(SubContainer)
 
 export { ElHeader, ElAside, ElMain, ElFooter, ElContainer }
 export default ElContainer
