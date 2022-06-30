d767729db7ac66925375dd8bd408113d8a39fb25
liyi <274195597@qq.com>
2022-06-30 21:10:00 +0800

    pref(ci): moudule path case error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from './components/button/'
 export { default as Tag } from './components/tag'
-export { default as Link } from './components/Link/'
+export { default as Link } from './components/link/'
 export { ElHeader, ElAside, ElMain, ElFooter, ElContainer } from './components/container/'
